8d1f87f39c47a55233d1213a603726c7bce9331b
quang <duongquangtrung1100@gmail.com>
2022-04-06 17:41:27 +0700

    seccond commit

---
+++ javaExample/src/com/example/helloworld/interFaceMain.java
@@ -0,0 +1,9 @@
+package com.example.helloworld;
+
+public class interFaceMain {
+    public static void main(String[] args) {
+        sinhVien sv1 = new sinhVien();
+        sv1.print();
+        sv1.scan();
+    }
+}
--- javaExample/src/com/example/helloworld/interfaceTest.java
+++ javaExample/src/com/example/helloworld/interfaceTest.java
@@ -1,26 +1,9 @@
 package com.example.helloworld;
 
-import java.util.Scanner;
-
 public interface interfaceTest {
     void print();
 }
 interface interfaceTest1 {
     void scan();
 }
-class  sinhVien implements interfaceTest, interfaceTest1{
-    public void print(){
-        System.out.println("ABC");
-    }
-    public void  scan(){
-        Scanner sc = new Scanner(System.in);
-        int i = sc.nextInt();
-        System.out.println(i);
-    }
 
-    public static void main(String[] args) {
-        sinhVien sv1 = new sinhVien();
-        sv1.print();
-        sv1.scan();
-    }
-}
